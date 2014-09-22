8bc94ff303f5997b331fb5173330c0d0bd3458f7
Camden Narzt <admin@silversshadows.com>
2014-09-22 12:03:34 -0600

    Initial Commit

---
+++ Barcodes2Go/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Barcodes2Go
+//
+//  Created by Camden Narzt on 2014-09-22.
+//  Copyright (c) 2014 Camden Narzt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Barcodes2Go/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Barcodes2Go
+//
+//  Created by Camden Narzt on 2014-09-22.
+//  Copyright (c) 2014 Camden Narzt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
