ee9f0a3789ae40a4788c433b20ce120456b5c763
Camden Narzt <admin@silversshadows.com>
2014-09-22 12:27:10 -0600

    Initial paste of code

--- Barcodes2Go/ViewController.m
+++ Barcodes2Go/ViewController.m
@@ -7,21 +7,62 @@
 //
 
 #import "ViewController.h"
+@import AVFoundation;
 
-@interface ViewController ()
+@interface ViewController () <AVCaptureMetadataOutputObjectsDelegate>
+
+@property (strong) AVCaptureSession *captureSession;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.captureSession = [[AVCaptureSession alloc] init];
+    AVCaptureDevice *videoCaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    AVCaptureDeviceInput *videoInput = [AVCaptureDeviceInput deviceInputWithDevice:videoCaptureDevice error:&error];
+    if(videoInput)
+        [self.captureSession addInput:videoInput];
+    else
+        NSLog(@"Error: %@", error);
+    
+    AVCaptureMetadataOutput *metadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    [self.captureSession addOutput:metadataOutput];
+    [metadataOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [metadataOutput setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code]];
+    
+    AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    previewLayer.frame = self.view.layer.bounds;
+    [self.view.layer addSublayer:previewLayer];
+    
+    [self.captureSession startRunning];
+}
+
+#pragma mark AVCaptureMetadataOutputObjectsDelegate
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    for(AVMetadataObject *metadataObject in metadataObjects)
+    {
+        AVMetadataMachineReadableCodeObject *readableObject = (AVMetadataMachineReadableCodeObject *)metadataObject;
+        if([metadataObject.type isEqualToString:AVMetadataObjectTypeQRCode])
+        {
+            NSLog(@"QR Code = %@", readableObject.stringValue);
+        }
+        else if ([metadataObject.type isEqualToString:AVMetadataObjectTypeEAN13Code])
+        {
+            NSLog(@"EAN 13 = %@", readableObject.stringValue);
+        }
+    }
 }
 
 @end
